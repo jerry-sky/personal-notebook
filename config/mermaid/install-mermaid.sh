a2fd189f46dcdca0f64cc6583f73535ba5bf33b5
jerry-sky <jwroczynski@gmail.com>
2020-03-11 23:19:15 +0100

    removed temporary measure for generating graphs

--- config/mermaid/install-mermaid.sh
+++
@@ -1,9 +0,0 @@
-# install the standard version
-cd ~/Downloads
-npm install mermaid.cli
-
-cp ~/notebooks/personal-notebook/config/mermaid/index.bundle.js ~/Downloads/node_modules/mermaid.cli/
-
-sudo ln -s ~/Downloads/node_modules/.bin/mmdc /usr/bin/mermaid
-
-echo "Done."
--- config/mermaid/readme.md
+++
@@ -1,9 +0,0 @@
-# Mermaid.cli
-
-## Notice
-
-**This is only a temporary measure for generating graph - using a modified version of mermaid.cli.**
-
-## Install
-
-Run `install-mermaid.sh`.
