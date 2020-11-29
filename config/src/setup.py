101b2282f4ddf7bacc48a885dc972fac59c5d676
jerry-sky <jwroczynski@gmail.com>
2020-11-29 18:23:34 +0100

    config: added the status indicator to the setup script rendered to the side of a given entry that tells if the entry has been installed or not

--- config/src/setup.py
+++ config/src/setup.py
@@ -1,22 +1,53 @@
 #!/usr/bin/env python3
 from sys import stdin, exit
 from config_entries.config_entries import config_entries
+from config_entries.config_entry import Status
 import pathlib
 import os
 
 EXIT_MESSAGE = '\033[3mbye\033[0m'
 
+INSTALLED_CHARACTER = '\033[38;2;95;217;97m✓'
+NOT_INSTALLED_CHARACTER = '\033[38;2;200;40;24m×'
+UNKNOWN_STATUS_CHARACTER = '\033[38;2;242;234;78m?'
+
 
 def print_available_options():
 
+    command_length = 0
+    for ce in config_entries:
+        t = len(ce.shorthand)
+        if t > command_length:
+            command_length = len(ce.shorthand)
+
+    print(command_length)
+
     print('\n\033[1mAvailable configs:\033[0m')
     for ce in config_entries:
-        print('  \033[38;5;244m×\033[0m \033[38;5;248m[\033[0m \033[38;5;135;1m' + ce.shorthand +
-              '\033[0m \033[38;5;248m] \033[38;5;244m→\033[0m \033[38;5;205m' + ce.description + '\033[0m')
+
+        is_installed = NOT_INSTALLED_CHARACTER
+        if ce.is_installed() == Status.Installed:
+            is_installed = INSTALLED_CHARACTER
+        elif ce.is_installed() == Status.Unknown:
+            is_installed = UNKNOWN_STATUS_CHARACTER
+
+        print('  \033[38;5;244m•\033[0m \033[38;5;248m[\033[0m \033[38;5;135;1m' + ce.shorthand + (' ' * (command_length - len(ce.shorthand)))\
+            + '\033[0m \033[38;5;248m] '\
+            + is_installed\
+            + '\033[0m \033[38;5;244m→\033[0m \033[38;5;205m' + ce.description + '\033[0m')
 
     print()
     print(
-        'Type the [ commands in brackets ] you want to execute splitted with whitespace or type `quit` to terminate this program.\n')
+        'Type the [ commands in brackets ] you want to execute splitted with whitespace or type `quit` to terminate this program.\n'\
+        + 'Type `l`, or `list` to view all available commands.\n'
+    )
+    print(
+        'Is installed status:\n',
+        '  ' + INSTALLED_CHARACTER + ' — installed\n',
+        '  ' + NOT_INSTALLED_CHARACTER + ' — not installed\n',
+        '  ' + UNKNOWN_STATUS_CHARACTER + ' — unknown status\n'
+        '\033[0m'
+    )
 
 
 if __name__ == "__main__":
