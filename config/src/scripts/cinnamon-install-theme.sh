b6491febe55e94d6a081a8a245248043fb0db43c
jerry-sky <jwroczynski@gmail.com>
2021-02-08 18:08:35 +0000

    deployed on 2021-02-08T18:08:34.392Z

---
+++ config/src/scripts/cinnamon-install-theme.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+printf "\n\033[1mInterface theme\nInstalling Materia GTK Theme…\033[0m\n"
+
+# install the base theme
+sudo apt-get install materia-gtk-theme
+
+# copy custom CSS into the installed GTK dark version of this theme suite
+cur_dir="${BASH_SOURCE%/*}"
+sudo cp "$cur_dir"/../../config-files/cinnamon-theme/cinnamon.css* -t "/usr/share/themes/Materia-dark/cinnamon"
+
+printf "\n\033[1mInstalling mouse cursor theme…\033[0m\n"
+# install mouse cursors
+cd /tmp
+git clone 'https://github.com/keeferrourke/capitaine-cursors'
+cd capitaine-cursors
+printf "\n\033[1mBuilding…\033[0m\n"
+sudo apt install inkscape x11-apps
+./build.sh -p unix -t light -d hd
+printf "\n\033[1mBuild complete\033[0m\n"
+sudo mkdir -p /usr/share/icons/Capitaine-Cursors-Light
+sudo mv dist/* /usr/share/icons/Capitaine-Cursors-Light
+cd "$cur_dir"
+
+printf "\n\033[1mConfiguring Cinnamon…\033[0m\n"
+# load appropriate Cinnamon interface settings
+dconf load /org/cinnamon/desktop/interface/ < "$cur_dir/../../config-files/cinnamon-theme/interface.conf"
