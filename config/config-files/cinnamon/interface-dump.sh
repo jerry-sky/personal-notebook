3cda3211869a9c9144668f987332719987fdde51
jerry-sky <jwroczynski@gmail.com>
2021-02-22 18:29:47 +0100

    config: cinnamon: fixed theming

--- config/config-files/cinnamon/interface-dump.sh
+++ config/config-files/cinnamon/interface-dump.sh
@@ -4,3 +4,5 @@ source "dconf-prefix.sh"
 
 dconf_prefix org/cinnamon/desktop/interface > interface.conf
 dconf_prefix org/cinnamon/desktop/wm/preferences >> interface.conf
+# don’t include the cache settings
+dconf_prefix org/cinnamon/theme | grep -v 'cache' >> interface.conf
