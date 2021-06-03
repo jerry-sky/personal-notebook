fdec47c7183786e1150f5e41964e4b8ebb298029
jerry-sky <jwroczynski@gmail.com>
2021-06-03 10:48:09 +0000

    deployed on 2021-06-03T10:48:08.200Z

---
+++ config/src/scripts/install-i3.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+printf "\n\033[1mi3-gaps\033[0m\n"
+
+printf "\n\033[1mInstalling…\033[0m\n"
+# add the PPA
+sudo add-apt-repository -y ppa:regolith-linux/stable
+sudo apt-get update
+# install the WM
+sudo apt-get install i3-gaps
+
+# install some utilities needed for making i3 more towards an actual DE
+printf "\n\033[1mInstalling utilities…\033[0m\n"
+# dex is needed for the GUI authentication prompt program (see the i3 config)
+sudo apt-get install -y dex
+# window compositor and background viewer
+sudo apt-get install -y compton feh
